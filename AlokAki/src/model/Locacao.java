99c0d804261bd3e41bc8187acf9798b2bdbf4a5a
jeanvinicius-r <jvr.classroom@gmail.com>
2025-11-24 21:52:17 -0300

    New: Ajustando o cliente no projeto

--- AlokAki/src/model/Locacao.java
+++ AlokAki/src/model/Locacao.java
@@ -5,65 +5,34 @@ import java.time.LocalDate;
 public class Locacao {
     private int id;
     private int idFilme;
+    private int idCliente;
     private LocalDate dataLocacao;
     private LocalDate dataPrevistaDevolucao;
-    private LocalDate dataDevolucao; 
+    private LocalDate dataDevolucao;
     private String status;
     private double valorDiaria;
 
-    public int getId() {
-        return id;
-    }
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public int getIdFilme() { return idFilme; }
+    public void setIdFilme(int idFilme) { this.idFilme = idFilme; }
 
-    public int getIdFilme() {
-        return idFilme;
-    }
+    public int getIdCliente() { return idCliente; }
+    public void setIdCliente(int idCliente) { this.idCliente = idCliente; }
 
-    public void setIdFilme(int idFilme) {
-        this.idFilme = idFilme;
-    }
+    public LocalDate getDataLocacao() { return dataLocacao; }
+    public void setDataLocacao(LocalDate dataLocacao) { this.dataLocacao = dataLocacao; }
 
-    public LocalDate getDataLocacao() {
-        return dataLocacao;
-    }
+    public LocalDate getDataPrevistaDevolucao() { return dataPrevistaDevolucao; }
+    public void setDataPrevistaDevolucao(LocalDate dataPrevistaDevolucao) { this.dataPrevistaDevolucao = dataPrevistaDevolucao; }
 
-    public void setDataLocacao(LocalDate dataLocacao) {
-        this.dataLocacao = dataLocacao;
-    }
+    public LocalDate getDataDevolucao() { return dataDevolucao; }
+    public void setDataDevolucao(LocalDate dataDevolucao) { this.dataDevolucao = dataDevolucao; }
 
-    public LocalDate getDataPrevistaDevolucao() {
-        return dataPrevistaDevolucao;
-    }
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
 
-    public void setDataPrevistaDevolucao(LocalDate dataPrevistaDevolucao) {
-        this.dataPrevistaDevolucao = dataPrevistaDevolucao;
-    }
-
-    public LocalDate getDataDevolucao() {
-        return dataDevolucao;
-    }
-
-    public void setDataDevolucao(LocalDate dataDevolucao) {
-        this.dataDevolucao = dataDevolucao;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public double getValorDiaria() {
-        return valorDiaria;
-    }
-
-    public void setValorDiaria(double valorDiaria) {
-        this.valorDiaria = valorDiaria;
-    }
+    public double getValorDiaria() { return valorDiaria; }
+    public void setValorDiaria(double valorDiaria) { this.valorDiaria = valorDiaria; }
 }
